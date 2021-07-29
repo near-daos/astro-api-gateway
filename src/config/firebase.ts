a540c77d7c19d86e9a0dfa6ee5c05a426c39b1c4
Oleg Kalenik <okalenyk@gmail.com>
2021-07-29 16:42:26 +0300

    *fixed firebase private key handling

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('firebase', () => ({
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 }));
