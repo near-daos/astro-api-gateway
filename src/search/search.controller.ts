9279f4673bac677361f4359ffd07b4730b9d6347
Oleg Kalenik <okalenyk@gmail.com>
2021-07-29 08:32:58 +0300

    *one more api ednpoint for swagger

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -5,7 +5,6 @@ import {
   Query, 
   UseInterceptors
 } from '@nestjs/common';
-import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { SearchQuery } from 'src/common';
 import { SearchResultDto } from './dto/search-result.dto';
 import { SearchService } from './search.service';
@@ -15,7 +14,6 @@ import { SearchService } from './search.service';
 export class SearchController {
   constructor(private readonly searchService: SearchService) { }
 
-  @ApiExcludeEndpoint()
   @Get('/search')
   async search(@Query() query: SearchQuery): Promise<SearchResultDto> {
     return await this.searchService.search(query)
