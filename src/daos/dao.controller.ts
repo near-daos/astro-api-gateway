5502eed8abbd2366d9c6ce69b53e550c6f30f0f5
Oleg Kalenik <okalenyk@gmail.com>
2021-07-29 08:31:56 +0300

    *included endpoints for swagger indexing

--- src/daos/dao.controller.ts
+++ src/daos/dao.controller.ts
@@ -7,7 +7,6 @@ import {
   Query,
   UseInterceptors
 } from '@nestjs/common';
-import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { FindOneParams, PagingQuery } from 'src/common';
 import { DaoService } from './dao.service';
 import { Dao } from './entities/dao.entity';
@@ -19,13 +18,11 @@ export class DaoController {
     private readonly daoService: DaoService
   ) { }
 
-  @ApiExcludeEndpoint()
   @Get('/daos')
   async daos(@Query() query: PagingQuery): Promise<Dao[]> {
     return await this.daoService.find(query);
   }
 
-  @ApiExcludeEndpoint()
   @Get('/daos/:id')
   async daoById(@Param() { id }: FindOneParams): Promise<Dao> {
     const dao: Dao = await this.daoService.findOne(id);
--- src/proposals/proposal.controller.ts
+++ src/proposals/proposal.controller.ts
@@ -7,7 +7,6 @@ import {
   Query,
   UseInterceptors
 } from '@nestjs/common';
-import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { FindOneParams, PagingQuery } from 'src/common';
 import { Proposal } from './entities/proposal.entity';
 import { ProposalService } from './proposal.service';
@@ -19,13 +18,11 @@ export class ProposalController {
     private readonly proposalService: ProposalService
   ) { }
 
-  @ApiExcludeEndpoint()
   @Get('/proposals')
   async proposals(@Query() query: PagingQuery): Promise<Proposal[]> {
     return await this.proposalService.find(query);
   }
 
-  @ApiExcludeEndpoint()
   @Get('/proposals/:id')
   async proposalById(@Param() { id }: FindOneParams): Promise<Proposal> {
     const proposal: Proposal = await this.proposalService.findOne(id);
