bf008c867c849643ff4d1a5cad3694d4b1d24c02
Oleg Kalenik <okalenyk@gmail.com>
2021-07-30 18:39:16 +0300

    *added notifications controller to swagger spec

--- src/notifications/dto/token.dto.ts
+++ src/notifications/dto/token.dto.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class TokenDto {
+
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   token: string;
