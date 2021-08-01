cab435858ccf2d39c1fbcf2064e20acf61e28548
Oleg Kalenik <okalenyk@gmail.com>
2021-08-01 17:27:16 +0300

    *fixed unit tests

--- src/notifications/notifications.service.spec.ts
+++ src/notifications/notifications.service.spec.ts
@@ -1,12 +1,29 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Token } from './entities/token.entity';
 import { NotificationsService } from './notifications.service';
 
 describe('NotificationsService', () => {
   let service: NotificationsService;
 
   beforeEach(async () => {
+    const mockRepository = jest.fn(() => ({
+      metadata: {
+        columns: [],
+        relations: [],
+      },
+    }));
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificationsService],
+      providers: [
+        NotificationsService, {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => (null))
+          }
+        },
+        { provide: getRepositoryToken(Token), useClass: mockRepository }],
     }).compile();
 
     service = module.get<NotificationsService>(NotificationsService);
