678dfa5cbe4a609532f1171b02f8e4d83668b10a
Oleg Kalenik <okalenyk@gmail.com>
2021-08-16 09:04:01 +0300

    *some code cleanup

--- src/subscriptions/subscription.module.ts
+++ src/subscriptions/subscription.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NEAR_INDEXER_DB_CONNECTION } from 'src/common/constants';
-import { TypeOrmConfigService } from 'src/config';
 import { nearProvider } from 'src/config/near';
-import { NearModule } from 'src/near/near.module';
 import { Subscription } from './entities/subscription.entity';
 import { NotificationsApiController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Subscription]),
-    TypeOrmModule.forRootAsync({
-      name: NEAR_INDEXER_DB_CONNECTION,
-      useClass: TypeOrmConfigService,
-    }),
-    NearModule
-  ],
+  imports: [TypeOrmModule.forFeature([Subscription])],
   controllers: [NotificationsApiController],
   providers: [SubscriptionService, nearProvider],
   exports: [SubscriptionService],
