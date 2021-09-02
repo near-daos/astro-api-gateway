e784cfac0fe6add3d9a4e1c436985805c795f371
Oleg Kalenik <okalenyk@gmail.com>
2021-09-02 08:48:36 +0300

    *switched to separate queues for notifications/cache clearance events

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,7 +1,8 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import {
-  EVENT_SERVICE,
   EVENT_CLEAR_HTTP_CACHE_MESSAGE_PATTERN,
+  EVENT_NOTIFICATION_SERVICE,
+  EVENT_CACHE_SERVICE,
 } from 'src/common/constants';
 import { ClientProxy } from '@nestjs/microservices';
 import { DaoUpdateMessage } from './messages/dao-update.message';
@@ -11,7 +12,12 @@ import { BaseMessage } from './messages/base.event';
 export class EventService {
   private readonly logger = new Logger(EventService.name);
 
-  constructor(@Inject(EVENT_SERVICE) private readonly client: ClientProxy) {}
+  constructor(
+    @Inject(EVENT_NOTIFICATION_SERVICE)
+    private readonly notificationEventClient: ClientProxy,
+    @Inject(EVENT_CACHE_SERVICE)
+    private readonly cacheEventClient: ClientProxy,
+  ) {}
 
   async handleDaoUpdates(daoIds: string[]): Promise<any> {
     return Promise.all([
@@ -21,18 +27,25 @@ export class EventService {
   }
 
   private async sendDaoUpdates(daoIds: string[]): Promise<void> {
-    return this.sendEvent(new DaoUpdateMessage(daoIds));
+    return this.sendEvent(
+      this.notificationEventClient,
+      new DaoUpdateMessage(daoIds),
+    );
   }
 
   private async sendClearCacheEvent(): Promise<void> {
     return this.sendEvent(
+      this.cacheEventClient,
       new BaseMessage(EVENT_CLEAR_HTTP_CACHE_MESSAGE_PATTERN, {}),
     );
   }
 
-  private async sendEvent({ pattern, data }: BaseMessage): Promise<void> {
+  private async sendEvent(
+    client: ClientProxy,
+    { pattern, data }: BaseMessage,
+  ): Promise<void> {
     try {
-      this.client.emit<any>(pattern, data);
+      client.emit<any>(pattern, data);
     } catch (error) {
       this.logger.error(error);
 
