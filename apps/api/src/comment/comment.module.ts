c261057a3087dbe7ea33b2086b61c7eaca43ce8c
Volodymyr Horin <vladimir.horin@magicpowered.io>
2022-01-25 12:23:15 +0200

    fix(import bounty module): added bounty module import to comment module

--- apps/api/src/comment/comment.module.ts
+++ apps/api/src/comment/comment.module.ts
@@ -9,6 +9,7 @@ import {
 import { DaoModule } from '@sputnik-v2/dao';
 import { ProposalModule } from '@sputnik-v2/proposal';
 import { EventModule } from '@sputnik-v2/event';
+import { BountyModule } from '@sputnik-v2/bounty';
 
 import { NearModule } from '../near/near.module';
 import { CommentsController } from './comment.controller';
@@ -16,6 +17,7 @@ import { CommentsController } from './comment.controller';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Comment, CommentReport]),
+    BountyModule,
     DaoModule,
     ProposalModule,
     EventModule,
