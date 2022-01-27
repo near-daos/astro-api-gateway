81f6ca5731bfe13a3f5840379bad80c32a5febce
Volodymyr Horin <vladimir.horin@magicpowered.io>
2022-01-27 19:45:32 +0200

    feat(bounty comment count): added bountyCommentsCount field to bounty context

--- libs/bounty/src/entities/bounty-context.entity.ts
+++ libs/bounty/src/entities/bounty-context.entity.ts
@@ -1,7 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
+import {
+  AfterLoad,
+  Column,
+  Entity,
+  getManager,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+} from 'typeorm';
 import { BaseEntity } from '@sputnik-v2/common';
 import { Proposal } from '@sputnik-v2/proposal/entities/proposal.entity';
+import { Comment } from '@sputnik-v2/comment/entities';
+import { CommentContextType } from '@sputnik-v2/comment/types';
 
 import { Bounty } from './bounty.entity';
 
@@ -22,4 +32,18 @@ export class BountyContext extends BaseEntity {
   @ApiProperty()
   @OneToOne(() => Bounty, (bounty) => bounty.bountyContext, { nullable: true })
   bounty: Bounty;
+
+  commentsCount: number;
+
+  @AfterLoad()
+  async countComments(): Promise<void> {
+    this.commentsCount = await getManager()
+      .createQueryBuilder(Comment, 'comment')
+      .where({
+        contextId: this.id,
+        contextType: CommentContextType.BountyContext,
+        isArchived: false,
+      })
+      .getCount();
+  }
 }
