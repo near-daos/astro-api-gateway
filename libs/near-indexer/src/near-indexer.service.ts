3f671d14e03d8758a64a491ef41e033ad3c91d8b
Volodymyr Horin <vladimir.horin@magicpowered.io>
2022-02-03 20:32:34 +0200

    fix(dao factory aggregation): refactor build quary usage

--- libs/near-indexer/src/near-indexer.service.ts
+++ libs/near-indexer/src/near-indexer.service.ts
@@ -375,9 +375,7 @@ export class NearIndexerService {
     contractNames: string[],
     fromBlockTimestamp: number,
   ): Promise<NFTTokenActionDto[]> {
-    const accountIds = contractNames.map((contractName) =>
-      buildLikeContractName(contractName),
-    );
+    const accountIds = contractNames.map(buildLikeContractName);
 
     const received = `
         select distinct receipt_receiver_account_id as nft, args->'args_json' as args, receipt_included_in_block_timestamp as timestamp
@@ -541,7 +539,8 @@ export class NearIndexerService {
       )
       .leftJoinAndSelect('receipts.receiptActions', 'action_receipt_actions')
       .where('account_change.affected_account_id like :id', {
-        id: buildLikeContractName(contractName, ''),
+        // Need to find all DAOs + factory contract changes
+        id: `%${contractName}`,
       })
       .andWhere('account_change.changed_in_block_timestamp > :from', {
         from: fromBlockTimestamp,
