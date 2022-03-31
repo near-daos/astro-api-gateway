a814272cb68fc14ecc42a55ddfcec5e233b30646
RomaShaMP <roman.shabanov93@gmail.com>
2022-03-31 18:14:43 +0300

    Was developed new test "Get list of created NFTs by DAO id"

--- test-framework/src/test/java/api/app/astrodao/com/tests/stats/StatsDaoNftApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/stats/StatsDaoNftApiTests.java
@@ -1,12 +1,17 @@
 package api.app.astrodao.com.tests.stats;
 
 import api.app.astrodao.com.core.dto.api.stats.StatsEntries;
+import api.app.astrodao.com.openapi.models.StatsEntryDto;
 import api.app.astrodao.com.steps.StatsApiSteps;
 import api.app.astrodao.com.tests.BaseTest;
 import io.qameta.allure.*;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 
@@ -21,18 +26,21 @@ public class StatsDaoNftApiTests extends BaseTest {
 
 	@Test
 	@Severity(SeverityLevel.CRITICAL)
-	@Story("Getting NFTs for a DAO")
-	@DisplayName("Getting NFTs for a DAO")
-	void gettingNFTsForDao() {
-		//TODO: Add steps to retrieve data for DAO from CLI
-		String dao = "gaming.sputnikv2.testnet";
+	@Story("Get list of created NFTs by DAO id")
+	@DisplayName("Get list of created NFTs by DAO id")
+	void getListOfCreatedNftsByDaoId() {
+		String daoId = "gaming.sputnikv2.testnet";
 
-		StatsEntries statsEntries = statsApiSteps.getBountiesForDao(dao).then()
+		StatsEntries statsEntries = statsApiSteps.getNFTsForDao(daoId).then()
 				.statusCode(HTTP_OK)
 				.extract().as(StatsEntries.class);
 
-		statsApiSteps.assertCollectionHasSizeGreaterThanOrEqualTo(statsEntries, 3);
-		statsApiSteps.assertCollectionElementsHasValue(statsEntries, r -> r.getValue().intValue() >= 0, "value");
-		statsApiSteps.assertCollectionElementsHasValue(statsEntries, r -> !r.getTimestamp().toString().isBlank(), "timestamp");
+		statsApiSteps.assertCollectionHasCorrectSize(statsEntries, 72);
+
+		List<BigDecimal> values1 = statsEntries.stream().map(StatsEntryDto::getValue).filter(bigDecimal -> bigDecimal.equals(BigDecimal.valueOf(0))).collect(Collectors.toList());
+		List<BigDecimal> values2 = statsEntries.stream().map(StatsEntryDto::getValue).filter(bigDecimal -> bigDecimal.equals(BigDecimal.valueOf(2))).collect(Collectors.toList());
+
+		statsApiSteps.assertCollectionHasCorrectSize(values1, 2);
+		statsApiSteps.assertCollectionHasCorrectSize(values2, 70);
 	}
 }
