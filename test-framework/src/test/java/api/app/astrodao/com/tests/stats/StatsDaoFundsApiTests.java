3c055a678cba63f2a35722d098a094d18b510820
RomaShaMP <roman.shabanov93@gmail.com>
2022-04-07 18:26:23 +0300

    Was updated test - "Getting funds for a DAO"

--- test-framework/src/test/java/api/app/astrodao/com/tests/stats/StatsDaoFundsApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/stats/StatsDaoFundsApiTests.java
@@ -6,9 +6,13 @@ import api.app.astrodao.com.tests.BaseTest;
 import io.qameta.allure.*;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
+import static org.hamcrest.Matchers.equalTo;
 
 @Tags({@Tag("all"), @Tag("statsApiTests"), @Tag("statsDaoFundsApiTests")})
 @Epic("Stats")
@@ -24,14 +28,13 @@ public class StatsDaoFundsApiTests extends BaseTest {
 	@Story("Getting funds for a DAO")
 	@DisplayName("Getting funds for a DAO")
 	void getFundsForDAO() {
-		//TODO: Add steps to retrieve data for DAO from CLI
 		String dao = "gaming.sputnikv2.testnet";
 
-		StatsEntries statsEntries = statsApiSteps.getBountiesForDao(dao).then()
+		StatsEntries statsEntries = statsApiSteps.getFundsForDao(dao).then()
 				.statusCode(HTTP_OK)
 				.extract().as(StatsEntries.class);
 
-		statsApiSteps.assertCollectionHasSizeGreaterThanOrEqualTo(statsEntries, 3);
+		statsApiSteps.assertCollectionHasSizeGreaterThanOrEqualTo(statsEntries, 79);
 		statsApiSteps.assertCollectionElementsHasValue(statsEntries, r -> r.getValue().intValue() >= 0, "value");
 		statsApiSteps.assertCollectionElementsHasValue(statsEntries, r -> !r.getTimestamp().toString().isBlank(), "timestamp");
 	}
