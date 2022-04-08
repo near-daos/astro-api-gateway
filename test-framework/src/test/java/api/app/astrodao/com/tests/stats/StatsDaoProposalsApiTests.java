3c03cbba516c5d86aa407adaeae74bbb5ba01c8b
RomaShaMP <roman.shabanov93@gmail.com>
2022-04-08 15:16:07 +0300

    Was created test - "Get HTTP 404 for DAO Proposals stats with invalid daoId"

--- test-framework/src/test/java/api/app/astrodao/com/tests/stats/StatsDaoProposalsApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/stats/StatsDaoProposalsApiTests.java
@@ -6,9 +6,13 @@ import api.app.astrodao.com.tests.BaseTest;
 import io.qameta.allure.*;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
+import static org.hamcrest.Matchers.equalTo;
 
 @Tags({@Tag("all"), @Tag("statsApiTests"), @Tag("statsDaoProposalsApiTests")})
 @Epic("Stats")
@@ -24,7 +28,6 @@ public class StatsDaoProposalsApiTests extends BaseTest {
 	@Story("Getting proposals for a DAO")
 	@DisplayName("Getting proposals for a DAO")
 	void gettingProposalsForDao() {
-		//TODO: Add steps to retrieve data for DAO from CLI
 		String dao = "gaming.sputnikv2.testnet";
 
 		ProposalStatsEntries statsEntries = statsApiSteps.getProposalsForDao(dao).then()
@@ -35,5 +38,18 @@ public class StatsDaoProposalsApiTests extends BaseTest {
 		statsApiSteps.assertCollectionElementsHasValue(statsEntries, r -> !r.getTimestamp().toString().isBlank(), "timestamp");
 		statsApiSteps.assertCollectionElementsHasValue(statsEntries, r -> r.getTotal().intValue() >= 0, "total");
 		statsApiSteps.assertCollectionElementsHasValue(statsEntries, r -> r.getActive().intValue() >= 0, "active");
+	}
+
+	@ParameterizedTest
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("Get HTTP 404 for DAO Proposals stats with invalid daoId")
+	@DisplayName("Get HTTP 404 for DAO Proposals stats with invalid daoId")
+	@CsvSource({"invalidDaoId", "2212332141", "-1", "0",
+			"*", "null", "autotest-dao-1.sputnikv2.testnet-1", "another-magic.near"})
+	void getHttp404ForDaoProposalsStatsWithInvalidDaoId(String invalidDaoId) {
+		statsApiSteps.getProposalsForDao(invalidDaoId).then()
+				.statusCode(HTTP_NOT_FOUND)
+				.body("statusCode", equalTo(404),
+				      "message", equalTo("Not Found"));
 	}
 }
