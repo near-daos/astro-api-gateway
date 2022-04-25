94676d9bf672b6d11d6c667dd0e98724d41a18a1
RomaShaMP <roman.shabanov93@gmail.com>
2022-04-25 11:39:32 +0300

    Was created new autotest on endpoint /api/v1/account/{id}

---
+++ test-framework/src/test/java/api/app/astrodao/com/steps/AccountApiSteps.java
@@ -0,0 +1,18 @@
+package api.app.astrodao.com.steps;
+
+import api.app.astrodao.com.core.annotations.Steps;
+import api.app.astrodao.com.core.controllers.AccountApi;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
+
+@Steps
+@RequiredArgsConstructor
+public class AccountApiSteps extends BaseSteps {
+	private final AccountApi accountApi;
+
+	@Step("Getting account settings by '{accountId}' query param")
+	public Response getAccountSettingsById(String accountId) {
+		return accountApi.getAccountSettingsById(accountId);
+	}
+}
