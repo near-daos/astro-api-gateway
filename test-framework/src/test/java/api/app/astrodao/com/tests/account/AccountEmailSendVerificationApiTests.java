6c3b2ce5bffb663eb152b4c35fea56a99cbb4a4d
RomaShaMP <roman.shabanov93@gmail.com>
2022-05-13 12:21:10 +0300

    Was added autotest "Get HTTP 403 for email verification with invalid 'publicKey' parameter"

---
+++ test-framework/src/test/java/api/app/astrodao/com/tests/account/AccountEmailSendVerificationApiTests.java
@@ -0,0 +1,50 @@
+package api.app.astrodao.com.tests.account;
+
+import api.app.astrodao.com.steps.AccountApiSteps;
+import api.app.astrodao.com.tests.BaseTest;
+import io.qameta.allure.*;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import static java.net.HttpURLConnection.HTTP_FORBIDDEN;
+import static org.hamcrest.Matchers.equalTo;
+
+@Tags({@Tag("all"), @Tag("accountEmailSendVerificationApiTests")})
+@Epic("Account")
+@Feature("/account/email/send-verification API tests")
+@DisplayName("/account/email/send-verification API tests")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AccountEmailSendVerificationApiTests extends BaseTest {
+	private final AccountApiSteps accountApiSteps;
+
+	@Value("${accounts.account3.accountId}")
+	private String accountId;
+
+	@Value("${accounts.account3.publicKey}")
+	private String accountPublicKey;
+
+	@Value("${accounts.account3.signature}")
+	private String accountSignature;
+
+	@ParameterizedTest
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("Get HTTP 403 for email verification with invalid 'publicKey' parameter")
+	@NullAndEmptySource
+	@CsvSource({"invalidPublicKey"})
+	void getHttp403ForValidationWithInvalidPublicKeyParam(String publicKey) {
+		accountApiSteps.sendEmailVerificationCode(accountId, publicKey, accountSignature)
+				.then()
+				.statusCode(HTTP_FORBIDDEN)
+				.body("statusCode", equalTo(HTTP_FORBIDDEN),
+				      "message", equalTo("Account astro-automation-reserved6.testnet identity is invalid - public key"),
+				      "error", equalTo("Forbidden"));
+	}
+}
