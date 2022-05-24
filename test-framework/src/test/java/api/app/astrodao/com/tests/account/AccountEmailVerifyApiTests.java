91a4dfa890fa798b9d5a0b20bcbdc3e33588e566
RomaShaMP <roman.shabanov93@gmail.com>
2022-05-24 10:22:18 +0300

    Added new test "Get HTTP 400 for account email verify with invalid numeric verification code"

--- test-framework/src/test/java/api/app/astrodao/com/tests/account/AccountEmailVerifyApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/account/AccountEmailVerifyApiTests.java
@@ -27,6 +27,9 @@ public class AccountEmailVerifyApiTests extends BaseTest {
 	@Value("${accounts.account1.token}")
 	private String account1token;
 
+	@Value("${accounts.account4.token}")
+	private String account4token;
+
 
 	@Test
 	@Severity(SeverityLevel.CRITICAL)
@@ -41,4 +44,20 @@ public class AccountEmailVerifyApiTests extends BaseTest {
 				      "message", equalTo("No email found for account: testdao2.testnet"),
 				      "error", equalTo("Bad Request"));
 	}
+
+	@Test
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("Get HTTP 400 for account email verify with invalid numeric verification code")
+	@DisplayName("Get HTTP 400 for account email verify with invalid numeric verification code")
+	void getHttp400ForAccountEmailVerifyWithInvalidNumericVerificationCode() {
+		String code = String.valueOf(faker.number().randomNumber(6, false));
+		String errorMessage = String.format("Invalid verification code: %s", code);
+
+		accountApiSteps.verifyEmail(account4token, code).then()
+				.statusCode(HTTP_BAD_REQUEST)
+				.body("statusCode", equalTo(HTTP_BAD_REQUEST),
+				      "message", equalTo(errorMessage),
+				      "error", equalTo("Bad Request"));
+	}
+
 }
