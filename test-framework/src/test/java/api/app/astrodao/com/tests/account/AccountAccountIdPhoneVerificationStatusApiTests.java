5f43612b6e658bcb51f409d9a02baca82e3f2e5b
RomaShaMP <roman.shabanov93@gmail.com>
2022-06-27 14:53:00 +0300

    New autotest "Get HTTP 400 for phone verification status for account without phone number"

--- test-framework/src/test/java/api/app/astrodao/com/tests/account/AccountAccountIdPhoneVerificationStatusApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/account/AccountAccountIdPhoneVerificationStatusApiTests.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static java.net.HttpURLConnection.HTTP_OK;
+import static org.hamcrest.Matchers.equalTo;
 
 @Tags({@Tag("all"), @Tag("accountAccountIdPhoneVerificationStatusApiTests")})
 @Epic("Account")
@@ -60,5 +62,19 @@ public class AccountAccountIdPhoneVerificationStatusApiTests extends BaseTest {
 		accountApiSteps.assertDtoValue(verificationStatus, VerificationStatus::getIsSend, false, "isSend");
 		accountApiSteps.assertDtoValueIsNull(verificationStatus, VerificationStatus::getCreatedAt, "createdAt");
 		accountApiSteps.assertDtoValueIsNull(verificationStatus, VerificationStatus::getTtl, "ttl");
+	}
+
+	@Test
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("Get HTTP 400 for phone verification status for account without phone number")
+	@DisplayName("Get HTTP 400 for phone verification status for account without phone number")
+	void getHttp400ForPhoneVerificationStatusForAccountWithoutPhoneNumber() {
+		String errorMessage = String.format("No phone number found for account: %s", accountId1);
+
+		accountApiSteps.getAccountPhoneVerificationStatus(accountId1).then()
+				.statusCode(HTTP_BAD_REQUEST)
+				.body("statusCode", equalTo(HTTP_BAD_REQUEST),
+				      "message", equalTo(errorMessage),
+				      "error", equalTo("Bad Request"));
 	}
 }
