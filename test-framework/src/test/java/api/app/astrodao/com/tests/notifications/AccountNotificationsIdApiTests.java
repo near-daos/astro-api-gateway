5a515cd15a7e91e38d8d948c810ca8074175dddb
RomaShaMP <roman.shabanov93@gmail.com>
2022-07-05 13:33:47 +0300

    New autotests "User should be able to mark as read account notification by notification id"

--- test-framework/src/test/java/api/app/astrodao/com/tests/notifications/AccountNotificationsIdApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/notifications/AccountNotificationsIdApiTests.java
@@ -70,4 +70,43 @@ public class AccountNotificationsIdApiTests extends BaseTest {
 		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getIsRead, "isRead");
 		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getNotification, "notification");
 	}
+
+	@Test
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("User should be able to mark as read account notification by notification id")
+	@DisplayName("User should be able to mark as read account notification by notification id")
+	void markAsReadAccountNotificationByNotificationId() {
+		Map<String, Object> queryParams = Map.of(
+				"limit", "1",
+				"s", String.format(
+						"{\"accountId\": \"%s\", \"isRead\":false}",
+						account2Id)
+		);
+
+		AccountNotificationResponse accountNotificationResponse = notificationsApiSteps.getAccountNotifications(queryParams).then()
+				.statusCode(HTTP_OK)
+				.extract()
+				.as(AccountNotificationResponse.class);
+
+		notificationsApiSteps.assertCollectionContainsOnly(accountNotificationResponse.getData(), AccountNotification::getIsRead, false, "data/isRead");
+		notificationsApiSteps.assertDtoValue(accountNotificationResponse, r -> r.getCount().intValue(), 1, "count");
+
+		String id = accountNotificationResponse.getData().get(0).getId();
+		String notificationId = accountNotificationResponse.getData().get(0).getNotificationId();
+		AccountNotification accountNotification = notificationsApiSteps.patchAccountNotificationsById(account2token, id, false, true, false).then()
+				.statusCode(HTTP_OK)
+				.extract().as(AccountNotification.class);
+
+		notificationsApiSteps.assertDtoValue(accountNotification, AccountNotification::getId, id, "id");
+		notificationsApiSteps.assertDtoValue(accountNotification, AccountNotification::getNotificationId, notificationId, "notificationId");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getIsArchived, "isArchived");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getCreatedAt, "createdAt");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getUpdatedAt, "updatedAt");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, accNotification -> accNotification.getAccountId().equals(account2Id), "accountId");
+		notificationsApiSteps.assertDtoValue(accountNotification, AccountNotification::getIsRead, true, "isRead");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getIsPhone, "isPhone");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getIsEmail, "isEmail");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getIsMuted, "isMuted");
+		notificationsApiSteps.assertDtoHasValue(accountNotification, AccountNotification::getNotification, "notification");
+	}
 }
