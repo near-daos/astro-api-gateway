38ad93cff352b4b5dbea4a1e103ab8902e3a4ef9
Volodymyr Horin <vladimir.horin@magicpowered.io>
2022-07-05 13:01:44 +0300

    fix(updated data missing): Include updatedAt + isArchived fields to response

--- libs/common/src/base.entity.ts
+++ libs/common/src/base.entity.ts
@@ -1,10 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, Column } from 'typeorm';
 
 export abstract class BaseEntity {
   @ApiProperty()
-  @Exclude()
   @Column({ type: 'boolean', default: false })
   isArchived: boolean;
 
@@ -16,7 +14,6 @@ export abstract class BaseEntity {
   createdAt: Date;
 
   @ApiProperty()
-  @Exclude()
   @UpdateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
