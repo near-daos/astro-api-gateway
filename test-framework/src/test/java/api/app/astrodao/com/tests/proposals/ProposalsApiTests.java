c940225304e26110ebe4f584ea004839cab116c1
RomaShaMP <roman.shabanov93@gmail.com>
2022-07-20 14:42:30 +0300

    Updated test "Get list of proposals with query param: [voted=false, accountId]"

--- test-framework/src/test/java/api/app/astrodao/com/tests/proposals/ProposalsApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/proposals/ProposalsApiTests.java
@@ -326,7 +326,7 @@ public class ProposalsApiTests extends BaseTest {
         proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> r.getProposalId() >= 0, "data/proposalId");
         proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> r.getDaoId().endsWith(".sputnikv2.testnet"), "data/daoId");
         proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> !r.getProposer().isEmpty(), "data/proposer");
-        proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> !r.getDescription().isEmpty(), "data/description");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> r.getDescription() != null, "data/description");
         proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> r.getStatus() != null, "data/status");
         proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> r.getVoteStatus() != null, "data/voteStatus");
         proposalsApiSteps.assertCollectionElementsHasValue(proposalResponse.getData(), r -> r.getKind() != null, "data/kind");
