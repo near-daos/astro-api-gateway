93d2a149fab7a9ee5d343edecb32b462c1e12cae
RomaShaMP <roman.shabanov93@gmail.com>
2022-08-08 11:50:07 +0300

    new test "Get list of draft-proposals with query param: [type, state]"

--- test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsApiTests.java
@@ -3,6 +3,7 @@ package api.app.astrodao.com.tests.draftservice.draftproposals;
 import api.app.astrodao.com.openapi.models.DraftPageResponse;
 import api.app.astrodao.com.openapi.models.DraftProposalBasicResponse;
 import api.app.astrodao.com.openapi.models.DraftProposalState;
+import api.app.astrodao.com.openapi.models.ProposalType;
 import api.app.astrodao.com.steps.draftservice.DraftProposalsApiSteps;
 import api.app.astrodao.com.tests.BaseTest;
 import io.qameta.allure.*;
@@ -68,7 +69,6 @@ public class DraftProposalsApiTests extends BaseTest {
 		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> !draftProposal.getCreatedAt().toString().isEmpty(), "createdAt");
 		draftProposalsApiSteps.assertCollectionElementsHasBooleanValueAndSize(draftProposalResponse.getData(), draftProposalBasicResponse -> !draftProposalBasicResponse.getIsRead(), "No boolean value for field:", "isRead");
 		draftProposalsApiSteps.assertCollectionElementsHasBooleanValueAndSize(draftProposalResponse.getData(), draftProposalBasicResponse -> !draftProposalBasicResponse.getIsSaved(), "No boolean value for field:", "isSaved");
-
 
 		List<OffsetDateTime> createdAtList = draftProposalResponse.getData().stream().map(DraftProposalBasicResponse::getCreatedAt).collect(Collectors.toList());
 		draftProposalsApiSteps.assertOffsetDateTimesAreSortedCorrectly(createdAtList, Comparator.reverseOrder(),
@@ -148,4 +148,42 @@ public class DraftProposalsApiTests extends BaseTest {
 		draftProposalsApiSteps.assertCollectionElementsHasBooleanValueAndSize(draftProposalResponse.getData(), DraftProposalBasicResponse::getIsRead, "No boolean value for field:", "isRead");
 		draftProposalsApiSteps.assertCollectionElementsHasBooleanValueAndSize(draftProposalResponse.getData(), DraftProposalBasicResponse::getIsSaved, "No boolean value for field:", "isSaved");
 	}
+
+	@Test
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("Get list of draft-proposals with query param: [type, state]")
+	@DisplayName("Get list of draft-proposals with query param: [type, state]")
+	void getListOfDraftProposalsWithQueryParamsTypeState() {
+		Map<String, Object> query = Map.of(
+				"type", "Transfer",
+				"state", "closed"
+		);
+
+		DraftPageResponse draftProposalResponse = draftProposalsApiSteps.getDraftProposals(query).then()
+				.statusCode(HTTP_OK)
+				.extract().as(DraftPageResponse.class);
+
+		draftProposalsApiSteps.assertDtoValue(draftProposalResponse, draftPageResponse -> draftPageResponse.getLimit().intValue(), 10, "limit");
+		draftProposalsApiSteps.assertDtoValue(draftProposalResponse, draftPageResponse -> draftPageResponse.getOffset().intValue(), 0, "offset");
+		draftProposalsApiSteps.assertDtoValueGreaterThanOrEqualTo(draftProposalResponse, draftPageResponse -> draftPageResponse.getTotal().intValue(), 9, "total");
+
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> !draftProposal.getId().isEmpty(), "id");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> !draftProposal.getDaoId().isEmpty(), "daoId");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> !draftProposal.getProposer().isEmpty(), "proposer");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> !draftProposal.getTitle().isEmpty(), "title");
+		draftProposalsApiSteps.assertCollectionContainsOnly(draftProposalResponse.getData(), DraftProposalBasicResponse::getType, ProposalType.TRANSFER, "type");
+		draftProposalsApiSteps.assertCollectionContainsOnly(draftProposalResponse.getData(), DraftProposalBasicResponse::getState, DraftProposalState.CLOSED, "state");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> draftProposal.getReplies().intValue() >= 0, "replies");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> draftProposal.getViews().intValue() >= 0, "views");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> draftProposal.getSaves().intValue() >= 0, "saves");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> !draftProposal.getUpdatedAt().toString().isEmpty(), "updatedAt");
+		draftProposalsApiSteps.assertCollectionElementsHasValue(draftProposalResponse.getData(), draftProposal -> !draftProposal.getCreatedAt().toString().isEmpty(), "createdAt");
+		draftProposalsApiSteps.assertCollectionElementsHasBooleanValueAndSize(draftProposalResponse.getData(), draftProposalBasicResponse -> !draftProposalBasicResponse.getIsRead(), "No boolean value for field:", "isRead");
+		draftProposalsApiSteps.assertCollectionElementsHasBooleanValueAndSize(draftProposalResponse.getData(), draftProposalBasicResponse -> !draftProposalBasicResponse.getIsSaved(), "No boolean value for field:", "isSaved");
+
+		List<OffsetDateTime> createdAtList = draftProposalResponse.getData().stream().map(DraftProposalBasicResponse::getCreatedAt).collect(Collectors.toList());
+		draftProposalsApiSteps.assertOffsetDateTimesAreSortedCorrectly(createdAtList, Comparator.reverseOrder(),
+		                                                               "'Order' param is not set. Draft proposals should be sorted by 'createdAt' field in DESC order by default");
+	}
+
 }
