bcb4f798326606dabdce2c0128df65994005dcd3
RomaShaMP <roman.shabanov93@gmail.com>
2022-08-22 16:33:23 +0300

    new test "Get HTTP 403 for draft proposal PATCH endpoint with null 'signature' parameter"

--- test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdApiTests.java
@@ -311,7 +311,7 @@ public class DraftProposalsIdApiTests extends BaseTest {
 	@Severity(SeverityLevel.NORMAL)
 	@Story("Get HTTP 403 for draft proposal PATCH endpoint with invalid 'signature' parameter")
 	@DisplayName("Get HTTP 403 for draft proposal PATCH endpoint with invalid 'signature' parameter")
-	void getHttp403ForDraftProposalPatchEndpointWithNullAndInvalidPublicKeyParam() {
+	void getHttp403ForDraftProposalPatchEndpointWithInvalidSignatureParam() {
 		String invalidSignature = account1Signature.substring(10);
 		String authToken = Base64Utils.encodeAuthToken(account1Id, account1PublicKey, invalidSignature);
 		String draftId = "62fdd6e98c658d00092a012d";
@@ -334,4 +334,33 @@ public class DraftProposalsIdApiTests extends BaseTest {
 				      "message", equalTo("Invalid signature"),
 				      "error", equalTo("Forbidden"));
 	}
+
+	@Test
+	@Severity(SeverityLevel.NORMAL)
+	@Story("Get HTTP 403 for draft proposal PATCH endpoint with null 'signature' parameter")
+	@DisplayName("Get HTTP 403 for draft proposal PATCH endpoint with null 'signature' parameter")
+	void getHttp403ForDraftProposalPatchEndpointWithNullSignatureParam() {
+		String authToken = Base64Utils.encodeAuthToken(account1Id, account1PublicKey, null);
+		String draftId = "62fdd6e98c658d00092a012d";
+
+		CreateDraftProposal createDraftProposal = new CreateDraftProposal();
+		createDraftProposal.setDaoId(testDao);
+		createDraftProposal.setTitle("Test title. Created " + getLocalDateTime());
+		createDraftProposal.setDescription("<p>" + faker.harryPotter().quote() + "</p>");
+		createDraftProposal.setType(ProposalType.VOTE);
+
+		ProposalKindSwaggerDto kind = new ProposalKindSwaggerDto();
+		kind.setType(ProposalKindSwaggerDto.TypeEnum.VOTE);
+		kind.setProposalVariant("ProposePoll");
+
+		createDraftProposal.setKind(kind);
+
+		draftProposalsApiSteps.updateDraftProposal(createDraftProposal, draftId, authToken).then()
+				.statusCode(HTTP_FORBIDDEN)
+				.body("statusCode", equalTo(HTTP_FORBIDDEN),
+				      "message", equalTo("Invalid signature"),
+				      "error", equalTo("Forbidden"));
+	}
+
+
 }
