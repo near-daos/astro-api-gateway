c023c6a12efbb928f221e6132ea7f5e36ae4a83f
RomaShaMP <roman.shabanov93@gmail.com>
2022-08-30 12:07:21 +0300

    New test "Get HTTP 403 for DELETE draft proposal save endpoint with null and invalid 'publicKey' parameter"

--- test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdSaveApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdSaveApiTests.java
@@ -211,4 +211,22 @@ public class DraftProposalsIdSaveApiTests extends BaseTest {
 				      "message", equalTo("Authorization header payload is invalid"),
 				      "error", equalTo("Forbidden"));
 	}
+
+	@ParameterizedTest
+	@Severity(SeverityLevel.NORMAL)
+	@Story("Get HTTP 403 for DELETE draft proposal save endpoint with null and invalid 'publicKey' parameter")
+	@DisplayName("Get HTTP 403 for DELETE draft proposal save endpoint with null and invalid 'publicKey' parameter")
+	@NullSource
+	@CsvSource({"invalidPublicKey"})
+	void getHttp403ForDeleteDraftProposalSaveEndpointWithNullAndInvalidPublicKeyParam(String publicKey) {
+		String authToken = Base64Utils.encodeAuthToken(account1Id, publicKey, account1Signature);
+		String errorMessage = String.format("Account %s identity is invalid - public key", account1Id);
+		String daoId = "63063c43a050fd00089b1f33";
+
+		draftProposalsApiSteps.unsaveDraftProposal(daoId, authToken).then()
+				.statusCode(HTTP_FORBIDDEN)
+				.body("statusCode", equalTo(HTTP_FORBIDDEN),
+				      "message", equalTo(errorMessage),
+				      "error", equalTo("Forbidden"));
+	}
 }
