20509d1eed6a0f8b61faa38ae44f43c591077842
RomaShaMP <roman.shabanov93@gmail.com>
2022-08-30 12:08:47 +0300

    New test "Get HTTP 403 for DELETE draft proposal save endpoint with empty 'publicKey' parameter"

--- test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdSaveApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdSaveApiTests.java
@@ -229,4 +229,19 @@ public class DraftProposalsIdSaveApiTests extends BaseTest {
 				      "message", equalTo(errorMessage),
 				      "error", equalTo("Forbidden"));
 	}
+
+	@Test
+	@Severity(SeverityLevel.NORMAL)
+	@Story("Get HTTP 403 for DELETE draft proposal save endpoint with empty 'publicKey' parameter")
+	@DisplayName("Get HTTP 403 for DELETE draft proposal save endpoint with empty 'publicKey' parameter")
+	void getHttp403ForDeleteDraftProposalSaveEndpointWithEmptyPublicKeyParam() {
+		String authToken = Base64Utils.encodeAuthToken(account1Id, EMPTY_STRING, account1Signature);
+		String daoId = "63063c43a050fd00089b1f33";
+
+		draftProposalsApiSteps.unsaveDraftProposal(daoId, authToken).then()
+				.statusCode(HTTP_FORBIDDEN)
+				.body("statusCode", equalTo(HTTP_FORBIDDEN),
+				      "message", equalTo("Authorization header payload is invalid"),
+				      "error", equalTo("Forbidden"));
+	}
 }
