a1996b2603617ffa286412b302ebe6e3538a474e
RomaShaMP <roman.shabanov93@gmail.com>
2022-09-06 12:39:56 +0300

    new test "Get HTTP 403 for draft proposal close endpoint with empty 'signature' parameter"

--- test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdCloseApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftproposals/DraftProposalsIdCloseApiTests.java
@@ -163,4 +163,19 @@ public class DraftProposalsIdCloseApiTests extends BaseTest {
 				      "message", equalTo("Invalid signature"),
 				      "error", equalTo("Forbidden"));
 	}
+
+	@Test
+	@Severity(SeverityLevel.NORMAL)
+	@Story("Get HTTP 403 for draft proposal close endpoint with empty 'signature' parameter")
+	@DisplayName("Get HTTP 403 for draft proposal close endpoint with empty 'signature' parameter")
+	void getHttp403ForDraftProposalCloseEndpointWithEmptySignatureParam() {
+		String authToken = Base64Utils.encodeAuthToken(account1Id, account1PublicKey, EMPTY_STRING);
+		String daoId = "63063c43a050fd00089b1f33";
+
+		draftProposalsApiSteps.closeDraftProposal(daoId, authToken).then()
+				.statusCode(HTTP_FORBIDDEN)
+				.body("statusCode", equalTo(HTTP_FORBIDDEN),
+				      "message", equalTo("Authorization header payload is invalid"),
+				      "error", equalTo("Forbidden"));
+	}
 }
