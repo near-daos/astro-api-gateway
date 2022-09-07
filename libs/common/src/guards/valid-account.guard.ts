fbec167b9b393219002a8b7a57f642a24b3d3144
Vova Horin <vhorin.dev@ip-192-168-1-2.ec2.internal>
2022-09-07 12:47:40 +0300

    fix(remove account validation): Remove account validation from ValidAccountGuard to avoid RPC requests

--- libs/common/src/guards/valid-account.guard.ts
+++ libs/common/src/guards/valid-account.guard.ts
@@ -2,33 +2,17 @@ import {
   BadRequestException,
   CanActivate,
   ExecutionContext,
-  Inject,
   Injectable,
-  NotFoundException,
 } from '@nestjs/common';
-import { NEAR_PROVIDER } from '@sputnik-v2/common/constants';
-import { Near } from 'near-api-js';
 
 @Injectable()
 export class ValidAccountGuard implements CanActivate {
-  constructor(
-    @Inject(NEAR_PROVIDER)
-    private near: Near,
-  ) {}
-
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     const accountId = req.params.accountId || req.body.accountId;
 
     if (!accountId) {
       throw new BadRequestException('Account ID is missing');
-    }
-
-    try {
-      const account = await this.near.account(accountId);
-      await account.state();
-    } catch (e) {
-      throw new NotFoundException(`Account does not exist: ${accountId}`);
     }
 
     return true;
