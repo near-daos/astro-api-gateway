b3587c63c666287b7a51d1d844871ced55a875ed
RomaShaMP <roman.shabanov93@gmail.com>
2022-09-21 14:38:31 +0300

    new test "Get list of draft comments with query param: [search, contextType, isReply=false]"

--- test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftcomments/DraftCommentsApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/draftservice/draftcomments/DraftCommentsApiTests.java
@@ -57,9 +57,9 @@ public class DraftCommentsApiTests extends BaseTest {
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getAuthor().isEmpty(), "author");
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getMessage().isEmpty(), "message");
 
-		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getReplies().isEmpty(), "replies");
-		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getLikeAccounts().isEmpty(), "likeAccounts");
-		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getDislikeAccounts().isEmpty(), "dislikeAccounts");
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getReplies() != null, "replies");
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getLikeAccounts() != null, "likeAccounts");
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getDislikeAccounts() != null, "dislikeAccounts");
 
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getCreatedAt().toString().isEmpty(), "createdAt");
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getUpdatedAt().toString().isEmpty(), "updatedAt");
@@ -156,8 +156,8 @@ public class DraftCommentsApiTests extends BaseTest {
 
 	@Test
 	@Severity(SeverityLevel.CRITICAL)
-	@Story("Get list of draft comments with query param: [orderBy, order, isReply]")
-	@DisplayName("Get list of draft comments with query param: [orderBy, order, isReply]")
+	@Story("Get list of draft comments with query param: [orderBy, order, isReply=true]")
+	@DisplayName("Get list of draft comments with query param: [orderBy, order, isReply=true]")
 	void getListOfDraftCommentsWithQueryParamsOrderByOrderIsReply() {
 		Map<String, Object> query = Map.of(
 				"orderBy", "updatedAt",
@@ -174,6 +174,7 @@ public class DraftCommentsApiTests extends BaseTest {
 		draftCommentsApiSteps.assertCollectionContainsOnly(draftCommentPageResponse.getData(), DraftCommentResponse::getContextType, DraftCommentResponse.ContextTypeEnum.DRAFTPROPOSAL, "contextType");
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getAuthor().isEmpty(), "author");
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getMessage().isEmpty(), "message");
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getReplyTo().isEmpty(), "message");
 
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getReplies().isEmpty(), "replies");
 		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getLikeAccounts().isEmpty(), "likeAccounts");
@@ -185,5 +186,86 @@ public class DraftCommentsApiTests extends BaseTest {
 		List<OffsetDateTime> createdAtList = draftCommentPageResponse.getData().stream().map(DraftCommentResponse::getCreatedAt).collect(Collectors.toList());
 		draftCommentsApiSteps.assertOffsetDateTimesAreSortedCorrectly(createdAtList, Comparator.naturalOrder(),
 		                                                              "Draft comments should be sorted by 'updatedAt' field in ASC order");
+	}
+
+	@Test
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("Get list of draft comments with query param: [search, contextType, isReply=false]")
+	@DisplayName("Get list of draft comments with query param: [search, contextType, isReply=false]")
+	void getListOfDraftCommentsWithQueryParamsContextTypeIsReply() {
+		String message = "automation test1";
+		String id = "6329dd483bbfd900084ded78";
+		String contextId = "6329dcd03bbfd900084ded75";
+		Map<String, Object> query = Map.of(
+				"search", message,
+				"isReply", "false",
+				"contextType", "DraftProposal"
+		);
+
+		DraftCommentPageResponse draftCommentPageResponse = draftCommentsApiSteps.getDraftComments(query).then()
+				.statusCode(HTTP_OK)
+				.extract().as(DraftCommentPageResponse.class);
+
+		draftCommentsApiSteps.assertCollectionContainsOnly(draftCommentPageResponse.getData(), DraftCommentResponse::getId, id, "id");
+		draftCommentsApiSteps.assertCollectionContainsOnly(draftCommentPageResponse.getData(), DraftCommentResponse::getContextId, contextId, "contextId");
+		draftCommentsApiSteps.assertCollectionContainsOnly(draftCommentPageResponse.getData(), DraftCommentResponse::getContextType, DraftCommentResponse.ContextTypeEnum.DRAFTPROPOSAL, "contextType");
+		draftCommentsApiSteps.assertCollectionContainsOnly(draftCommentPageResponse.getData(), DraftCommentResponse::getAuthor, "sputnikautomation1.testnet", "author");
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> draftCommentResponse.getMessage().contains(message), "message");
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getReplies().isEmpty(), "replies");
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().allMatch(
+						draftCommentReply ->
+								draftCommentReply.getContextId().equals(contextId)), "replies/contextId");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().allMatch(
+						draftCommentReply ->
+								draftCommentReply.getContextType().equals(DraftCommentResponse.ContextTypeEnum.DRAFTPROPOSAL)), "replies/contextType");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().allMatch(
+						draftCommentReply ->
+								draftCommentReply.getAuthor().equals("sputnikautomation1.testnet")), "replies/author");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().allMatch(
+						draftCommentReply ->
+								draftCommentReply.getMessage().contains("automation test2")), "replies/message");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().allMatch(
+						draftCommentReply ->
+								draftCommentReply.getReplyTo().equals(id)), "replies/replyTo");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().allMatch(
+						draftCommentReply ->
+								draftCommentReply.getLikeAccounts().isEmpty()), "replies/likeAccounts");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().noneMatch(
+						draftCommentReply ->
+								draftCommentReply.getCreatedAt().toString().isEmpty()), "replies/createdAt");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftComment ->
+				draftComment.getReplies().stream().noneMatch(
+						draftCommentReply ->
+								draftCommentReply.getUpdatedAt().toString().isEmpty()), "replies/updatedAt");
+
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getLikeAccounts().isEmpty(), "likeAccounts");
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getDislikeAccounts().isEmpty(), "dislikeAccounts");
+
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getCreatedAt().toString().isEmpty(), "createdAt");
+		draftCommentsApiSteps.assertCollectionElementsHasValue(draftCommentPageResponse.getData(), draftCommentResponse -> !draftCommentResponse.getUpdatedAt().toString().isEmpty(), "updatedAt");
 	}
 }
