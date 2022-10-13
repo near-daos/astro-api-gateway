c334d604360e025f342fe5f2e614357208363c89
RomaShaMP <roman.shabanov93@gmail.com>
2022-10-13 11:30:53 +0300

    Test updated "Get list of proposals with query param: [limit, orderBy, order=ASC, active=true]"

--- test-framework/src/test/java/api/app/astrodao/com/tests/apiservice/proposals/ProposalsApiTests.java
+++ test-framework/src/test/java/api/app/astrodao/com/tests/apiservice/proposals/ProposalsApiTests.java
@@ -42,8 +42,8 @@ public class ProposalsApiTests extends BaseTest {
 
     @Test
     @Severity(SeverityLevel.CRITICAL)
-    @Story("Get list of proposals with query param: [limit, offset, orderBy, order]")
-    @DisplayName("Get list of proposals with query param: [limit, offset, orderBy, order]")
+    @Story("Get list of proposals with query param: [limit, offset, orderBy, order=DESC]")
+    @DisplayName("Get list of proposals with query param: [limit, offset, orderBy, order=DESC]")
     void getListOfProposalsWithLimitOffsetOrderByOrderParams() {
         int limit = 10;
 
@@ -116,7 +116,7 @@ public class ProposalsApiTests extends BaseTest {
     @Severity(SeverityLevel.CRITICAL)
     @Story("Get list of proposals with query param: [dao, status, type, proposer]")
     @DisplayName("Get list of proposals with query param: [dao, status, type, proposer]")
-    void getListOfProposalsWithQueryParamsDaoStatusTypeProposerActiveFailed() {
+    void getListOfProposalsWithQueryParamsDaoStatusTypeProposerFailed() {
         Map<String, Object> query = Map.of(
                 "dao", testDao,
                 "status", "InProgress",
@@ -241,6 +241,78 @@ public class ProposalsApiTests extends BaseTest {
         proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getCanReject(), Boolean.TRUE, "data/permissions/canReject");
         proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getCanDelete(), Boolean.TRUE, "data/permissions/canDelete");
         proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getCanAdd(), Boolean.TRUE, "data/permissions/canAdd");
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Get list of proposals with query param: [limit, orderBy, order=ASC, active=true]")
+    @DisplayName("Get list of proposals with query param: [limit, orderBy, order=ASC, active=true]")
+    void getListOfProposalsWithQueryParamsLimitOrderByOrderActive() {
+        int limit = 10;
+        Map<String, Object> query = Map.of(
+                "limit", limit,
+                "orderBy", "id",
+                "order", "ASC",
+                "active", true
+        );
+
+        ProposalsResponse proposalsResponse = proposalsApiSteps.getProposals(query).then()
+                .statusCode(HTTP_OK)
+                .extract().as(ProposalsResponse.class);
+
+
+        proposalsApiSteps.assertDtoValueGreaterThan(proposalsResponse, r -> r.getTotal().intValue(), limit, "total");
+        proposalsApiSteps.assertDtoValue(proposalsResponse, r -> r.getOffset().intValue(), 0, "offset");
+        proposalsApiSteps.assertDtoValueGreaterThan(proposalsResponse, r -> r.getTotal().intValue(), 185, "total");
+        proposalsApiSteps.assertCollectionHasCorrectSize(proposalsResponse.getData(), limit);
+
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getCreatedAt().toString().isEmpty(), "data/createdAt");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getUpdatedAt().toString().isEmpty(), "data/updatedAt");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getTransactionHash().isEmpty(), "data/transactionHash");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getId().contains(".sputnikv2.testnet-"), "data/id");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getProposalId().intValue() >= 0, "data/proposalId");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getDaoId().endsWith(".sputnikv2.testnet"), "data/daoId");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getProposer().isEmpty(), "data/proposer");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDescription().isEmpty(), "data/description");
+        proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), Proposal::getStatus, Proposal.StatusEnum.INPROGRESS, "data/status");
+        proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), Proposal::getVoteStatus, Proposal.VoteStatusEnum.ACTIVE, "data/voteStatus");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getKind() != null, "data/kind");
+
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getType().toString().isEmpty(), "data/type");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getVotes() != null, "data/votes");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getVotePeriodEnd().toString().isEmpty(), "data/votePeriodEnd");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getCommentsCount().intValue() >= 0, "data/commentsCount");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getActions() != null, "data/actions");
+//
+        proposalsApiSteps.assertDeepCollectionElementsMatchCondition(proposalsResponse.getData(), Proposal::getActions, proposal -> !proposal.getId().isEmpty(), "data/actions/id", "not empty");
+        proposalsApiSteps.assertDeepCollectionElementsMatchCondition(proposalsResponse.getData(), Proposal::getActions, proposal -> !proposal.getProposalId().isEmpty(), "data/actions/proposalId", "not empty");
+        proposalsApiSteps.assertDeepCollectionElementsMatchCondition(proposalsResponse.getData(), Proposal::getActions, proposal -> !proposal.getAccountId().isEmpty(), "data/actions/accountId", "not empty");
+        proposalsApiSteps.assertDeepCollectionElementsMatchCondition(proposalsResponse.getData(), Proposal::getActions, proposal -> proposal.getAction() != null, "data/actions/action", "!= null");
+        proposalsApiSteps.assertDeepCollectionElementsMatchCondition(proposalsResponse.getData(), Proposal::getActions, proposal -> !proposal.getTransactionHash().isEmpty(), "data/actions/transactionHash", "not empty");
+        proposalsApiSteps.assertDeepCollectionElementsMatchCondition(proposalsResponse.getData(), Proposal::getActions, proposal -> !proposal.getTimestamp().toString().isEmpty(), "data/actions/timestamp", "not empty");
+
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getDao() != null, "data/dao");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getTransactionHash().isEmpty(), "data/dao/transactionHash");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getId().isEmpty(), "data/dao/id");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getConfig().getName().isEmpty(), "data/dao/config/name");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getDao().getConfig().getPurpose() != null, "data/dao/config/purpose");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getConfig().getMetadata().isEmpty(), "data/dao/config/metadata");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getDao().getNumberOfMembers().intValue() >= 1, "data/dao/numberOfMembers");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getPolicy().getDaoId().isEmpty(), "data/dao/policy/daoId");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getPolicy().getDefaultVotePolicy().getWeightKind().toString().isEmpty(), "data/dao/policy/defaultVotePolicy/weightKind");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> r.getDao().getPolicy().getDefaultVotePolicy().getQuorum().intValue() >= 0, "data/dao/policy/defaultVotePolicy/quorum");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getPolicy().getDefaultVotePolicy().getKind().toString().isEmpty(), "data/dao/policy/defaultVotePolicy/kind");
+        proposalsApiSteps.assertCollectionElementsHasValue(proposalsResponse.getData(), r -> !r.getDao().getPolicy().getDefaultVotePolicy().getRatio().isEmpty(), "data/dao/policy/defaultVotePolicy/ratio");
+
+        proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getIsCouncil(), Boolean.FALSE, "data/permissions/isCouncil");
+        proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getCanApprove(), Boolean.FALSE, "data/permissions/canApprove");
+        proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getCanReject(), Boolean.FALSE, "data/permissions/canReject");
+        proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getCanDelete(), Boolean.FALSE, "data/permissions/canDelete");
+        proposalsApiSteps.assertCollectionContainsOnly(proposalsResponse.getData(), proposal -> proposal.getPermissions().getCanAdd(), Boolean.FALSE, "data/permissions/canAdd");
+
+        List<String> listOfIds = proposalsResponse.getData().stream().map(Proposal::getId).collect(Collectors.toList());
+        proposalsApiSteps.assertStringsAreSortedCorrectly(listOfIds, Comparator.naturalOrder(),
+                                                                  "Proposals should be sorted by 'id' field in ASC order");
     }
 
     @ParameterizedTest
