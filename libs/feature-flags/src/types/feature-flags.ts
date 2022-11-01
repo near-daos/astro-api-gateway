9bf643927663e2e00db4e754b9e2c530ff0ca583
Vova Horin <vhorin.dev@ip-192-168-1-6.eu-west-1.compute.internal>
2022-11-01 18:59:15 +0200

    feat(draft proposal toggle): Add feature flag for draft proposal opensearch indexing

--- libs/feature-flags/src/types/feature-flags.ts
+++ libs/feature-flags/src/types/feature-flags.ts
@@ -2,4 +2,5 @@ export enum FeatureFlags {
   OpenSearchProposalIndexing = 'open-search-proposal-indexing',
   OpenSearchDaoIndexing = 'open-search-dao-indexing',
   OpenSearchBountyIndexing = 'open-search-bounty-indexing',
+  OpenSearchDraftProposalIndexing = 'open-search-draft-proposal-indexing',
 }
