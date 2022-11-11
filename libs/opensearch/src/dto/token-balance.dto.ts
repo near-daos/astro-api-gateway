dfb8a1fee91b02ff96963897d2e55fbf288d0695
Vova Horin <vhorin.dev@MacBook-Pro-Volodymyr.local>
2022-11-11 16:08:16 +0200

    feat(token price opensearch)

---
+++ libs/opensearch/src/dto/token-balance.dto.ts
@@ -0,0 +1,20 @@
+import { BaseOpensearchDto } from './base-opensearch.dto';
+
+export class TokenPriceOpensearchDto extends BaseOpensearchDto {
+  public static getMappings(): any {
+    const { mappings } = super.getMappings();
+    const { properties } = mappings;
+
+    return {
+      mappings: {
+        ...mappings,
+        properties: {
+          ...properties,
+          tokenId: { type: 'keyword' },
+          name: { type: 'text' },
+          symbol: { type: 'text' },
+        },
+      },
+    };
+  }
+}
