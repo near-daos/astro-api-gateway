b7a5df9c340ab2e230e5a943262d0f2680ff6cf5
andkom <andkom@users.noreply.github.com>
2022-11-24 12:30:33 +0200

    dynamo types fix

--- libs/dynamodb/src/models/base.model.ts
+++ libs/dynamodb/src/models/base.model.ts
@@ -2,7 +2,7 @@ import { BaseEntity } from '../types';
 
 export class BaseModel extends BaseEntity {
   isArchived: boolean;
-  processingTimeStamp: number;
   // TODO: use bigint and timestamp with nanoseconds
   createTimestamp: number;
+  processingTimeStamp: number;
 }
